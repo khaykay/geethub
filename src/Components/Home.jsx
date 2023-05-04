0c82dacde67534f17a2d4a7ac1d89ccaf94a0942
khaykay <berryscott97@gmail.com>
2023-05-04 01:03:14 +0100

    adding more styles

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Navbar from "./Navbar";
 import "../styles/Home.css";
+import Main from "./Main";
+import Profile from "./Profile";
 
 const Home = () => {
   return (
     <>
       <div className="home_wrapper">
         <Navbar />
-        Home
+        <Main />
+        <Profile />
       </div>
     </>
   );
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,25 +6,21 @@ const Navbar = () => {
     <>
       <div className="Nav_container">
         <div className="logo">
-          <img src={githublogo} alt="ReactImage" />
+          <img className="github_logo" src={githublogo} alt="ReactImage" />
         </div>
         <div className="overview_logo logo">
           <i class="material-symbols-outlined">import_contacts</i>
         </div>
         <div className="repo_logo logo">
-          Repositories
           <i class="material-symbols-outlined">collections_bookmark</i>
         </div>
         <div className="project_logo logo">
-          Projects
           <i class="material-symbols-outlined">dashboard</i>
         </div>
         <div className="packages_logo logo">
-          Packages
           <i class="material-symbols-outlined">inventory_2</i>
         </div>
         <div className="stars_logo logo">
-          stars
           <i class="material-symbols-rounded">star</i>
         </div>
       </div>
