aa69988e5f2dc2191857079973cea8b230bc9ee6
khaykay <berryscott97@gmail.com>
2023-06-01 16:25:52 +0100

    overview styles

---
+++ src/Components/Bar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "../styles/Bar.css";
+
+const Bar = () => {
+  return (
+    <div className="bar_wrapper">
+      <div className=""></div>
+      <div className="bar">
+        <div className="nav_icon">
+          <div className="overview_logo logo">
+            <i class="material-symbols-outlined">import_contacts</i>
+            <span className="logo_title">Overview</span>
+          </div>
+          <div className="repo_logo logo">
+            <i class="material-symbols-outlined">collections_bookmark</i>
+            <span className="logo_title">Repositories</span>
+          </div>
+          <div className="project_logo logo">
+            <i class="material-symbols-outlined">dashboard</i>
+            <span className="logo_title">Projects</span>
+          </div>
+          <div className="packages_logo logo">
+            <i class="material-symbols-outlined">inventory_2</i>
+            <span className="logo_title">Packages</span>
+          </div>
+          <div className="stars_logo logo">
+            <i class="material-symbols-rounded">star</i>
+            <span className="logo_title">Stars</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Bar;
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../styles/Main.css";
 import Profile from "./Profile";
+import Bar from "./Bar";
 
 const Main = ({ data }) => {
   const { user } = data;
@@ -9,19 +10,33 @@ const Main = ({ data }) => {
   return (
     <>
       <div className="main_wrapper">
-        <Profile data={data} />
-        <div className="overview">
-          <div className="pinned">
-            {pinnedItems.map((items) => {
-              return (
-                <div className="pinned_items" key={items.id}>
-                  <i class="material-symbols-outlined">collections_bookmark</i>
-                  <a href={items.url}>{items.name}</a>
-                  <span>{items.languages.edges[0].node.color}</span>
-                  <span>{items.languages.edges[0].node.name}</span>
-                </div>
-              );
-            })}
+        <Bar />
+        <div className="layout_wrapper">
+          <div className="layout">
+            <Profile data={data} />
+            <div className="overview">
+              <div className="pinned">
+                {pinnedItems.map((items) => {
+                  const style = {
+                    color: items.languages.edges[0].node.color,
+                  };
+                  return (
+                    <div className="pinned_items_wrapper" key={items.id}>
+                      <div className="pinned_items">
+                        <i class="material-symbols-outlined">
+                          collections_bookmark
+                        </i>
+                        <a href={items.url}>{items.name}</a>
+                        <span classname="" style={{ color: style.color }}>
+                          {items.languages.edges[0].node.color}
+                        </span>
+                        <span>{items.languages.edges[0].node.name}</span>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
           </div>
         </div>
       </div>
